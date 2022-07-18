f5c80dc547675832eb3b767ec62f9a8bf787518a
Cristopher Steve <63527137+FlowDistant@users.noreply.github.com>
2022-07-18 15:13:56 -0400

    Update Item.ts

--- src/lib/core/Item.ts
+++ src/lib/core/Item.ts
@@ -1,9 +1,7 @@
 export class Item extends app.Entity {
   constructor(address: bigint,
     readonly isLocal: boolean,
-    readonly itemId = new app.UInt8Pointer(address + entityOffsets.itemId),
-    readonly cPropSurvival = new app.UInt64Pointer(address + entityOffsets.cPropSurvival),
-    readonly m_iSignifierName = new app.CStringPointer(address + entityOffsets.m_iSignifierName, 32)
+    readonly itemId = new app.UInt8Pointer(address + entityOffsets.itemId)
     ) {
     super(address);
   }
