e249d96101b61c37f5f08368083e6e9baa15ce61
Cristopher Steve <63527137+FlowDistant@users.noreply.github.com>
2022-07-18 15:26:11 -0400

    Delete Item.ts

--- src/lib/core/Item.ts
+++
@@ -1,7 +0,0 @@
-export class Item extends app.Entity {
-  constructor(address: bigint,
-    readonly isLocal: boolean,
-    readonly itemId = new app.UInt8Pointer(address + entityOffsets.itemId)
-    ) {
-    super(address);
-  }
