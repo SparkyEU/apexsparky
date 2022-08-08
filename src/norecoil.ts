f7d3106c48415d301f52c6b65876b08092145e82
SparkyEU <109250847+SparkyEU@users.noreply.github.com>
2022-08-08 23:33:25 +0100

    Update norecoil.ts

--- src/norecoil.ts
+++ src/norecoil.ts
@@ -2,17 +2,20 @@ import * as app from './lib';
 import {ui} from './ui';
 const container = <HTMLElement> document.querySelector('.container');
 const content = <HTMLElement> document.querySelector('.content');
+const frameTime = 1000 / 10;
 
 ui((x) => {
   container.style.display = 'inherit';
   content.textContent = 'Norecoil running. Keep this window open.';
-  return renderAsync(x, new app.features.Norecoil());
+  return renderAsync(x, new app.Norecoil(x));
 });
 
-async function renderAsync(core: app.core.Core, norecoil: app.features.Norecoil) {
-  await core.runAsync(() => {
-    const players = core.entityList.value;
-    const localPlayer = players.find(x => x.address === core.localPlayer.value);
-    if (localPlayer) norecoil.updateStates(localPlayer);
-  });
+async function renderAsync(core: app.Core, sense: app.Norecoil) {
+  while (true) {
+    const beginTime = Date.now();
+    const players = await core.playersAsync();
+    const localPlayer = players.find(x => x.isLocal);
+    if (localPlayer) await sense.updateAsync(localPlayer, players);
+    await new Promise(x => setTimeout(x, frameTime - (Date.now() - beginTime)));
+  }
 }
