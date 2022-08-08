6518481ec85be7297bbe10ff44956ce979612922
SparkyEU <109250847+SparkyEU@users.noreply.github.com>
2022-08-08 22:39:58 +0100

    Update entityOffsets.ts

--- src/lib/core/offsets/entityOffsets.ts
+++ src/lib/core/offsets/entityOffsets.ts
@@ -5,5 +5,10 @@ export const entityOffsets = {
   glowColor: BigInt('0x1d0'),
   glowType: BigInt('0x2c4'),
   glowEnable: BigInt('0x3c8'),
-  glowThroughWall: BigInt('0x3d0')
+  glowThroughWall: BigInt('0x3d0'),
+  visible_time: BigInt('0x1ad0'),
+  itemId: BigInt('0x16a8'),
+  itemGlow: BigInt('0x2c0'),
+  glowMode: BigInt('0x2c4'),
+  Signifier: BigInt('0x580')
 };
